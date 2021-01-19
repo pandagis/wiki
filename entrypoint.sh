11b4ca3436cedf7237f7596b5be559573fb9972b
pandagis <pandagis@qq.com>
2021-01-19 15:51:13 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 创建数据库
-echo "创建数据库：${DB_NAME}"
+#echo "创建数据库：${DB_NAME}"
 echo "create database if not exists ${DB_NAME}" | postgres -h ${DB_HOST} -P ${DB_PORT} -u ${DB_USER} -p${DB_PASS}
 echo "创建数据库成功：${DB_NAME}"
 
